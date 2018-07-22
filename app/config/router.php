5a02d93b89419e571e566580fc48e507139e6109
Nick <398084103@qq.com>
2018-07-22 14:48:21 +0800

    Server Request Uri

--- app/config/router.php
+++ app/config/router.php
@@ -1,6 +1,11 @@
 <?php
+use Phalcon\Mvc\Router;
 
 $router = $di->getRouter();
+//Use $_GET['_url'] (default)
+//$router->setUriSource(Router::URI_SOURCE_GET_URL);
+//Use $_SERVER['REQUEST_URI']
+$router->setUriSource(Router::URI_SOURCE_SERVER_REQUEST_URI);
 
 // Define your routes here
 
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,11 +1,19 @@
 <?php
+use Phalcon\Mvc\Controller;
 
-class IndexController extends ControllerBase
+class IndexController extends Controller
 {
 
     public function indexAction()
     {
+        $this->view->disable();
+        echo "Hello World!";
 
+    }
+
+    public function hiAction()
+    {
+        echo __FILE__ . '<br>' . __METHOD__;
     }
 
 }
